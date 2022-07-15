336467585b756ad0555be50c35bc509b40bc14f1
mborsch <matthewmborschnack@gmail.com>
2022-07-14 22:08:45 -0500

    added createReservation async function

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -58,6 +58,17 @@ async function fetchJson(url, options, onCancel) {
  *  a promise that resolves to a possibly empty array of reservation saved in the database.
  */
 
+export async function createReservation(reservation, signal) {
+  const url = `${API_BASE_URL}/reservations`;
+  const options = {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ data: reservation }),
+    signal,
+  };
+  return await fetchJson(url, options);
+}
+
 export async function listReservations(params, signal) {
   const url = new URL(`${API_BASE_URL}/reservations`);
   Object.entries(params).forEach(([key, value]) =>
