63fa256081fe5bac7ff545036fb608b3a8f76ad2
mborsch <matthewmborschnack@gmail.com>
2022-07-15 21:37:45 -0500

    added date and time validator

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -6,7 +6,7 @@ const VALID_PROPERTIES = [
   "first_name",
   "last_name",
   "mobile_number",
-  "reservation_data",
+  "reservation_date",
   "reservation_time",
   "people",
 ];
@@ -27,10 +27,63 @@ function hasOnlyValidProperties(req, res, next) {
   next();
 }
 
-async function list(req, res) {
-  const data = await service.list(req.query.date);
+/*
+async function reservationValidation(req, res, next) {
+  if (!req.body.data) {
+    return next({ status: 400, message: `Data missing!`})
+  }
+  const {
+    first_name,
+    last_name,
+    mobile_number,
+    reservation_date,
+    reservation_time,
+    people,
+  } = req.body.data;
 
-  res.json({ data });
+}
+*/
+
+async function hasValidTimeAndDate(req, res, next) {
+  const { reservation_date, reservation_time } = req.body.data;
+  const date = new Date(reservation_date);
+  const today = new Date();
+  const reservationDate = new Date(reservation_date).toUTCString();
+
+  if (reservationDate.includes("Tue")) {
+    return next({
+      status: 400,
+      message: "Sorry, we are closed on Tuesday. Please choose another day.",
+    });
+  }
+
+  if (
+    date.valueOf() < today.valueOf() &&
+    date.toUTCString().slice(0, 16) !== today.toUTCString().slice(0, 16)
+  ) {
+    return next({
+      status: 400,
+      message: "Reservations must be made in the future.",
+    });
+  }
+
+  if (reservation_time < "10:30" || reservation_time > "21:30") {
+    return next({
+      status: 400,
+      message:
+        "The restaurant accepts reservations between 10:30 AM and 9:30 PM. Please choose another time.",
+    });
+  }
+
+  next();
+}
+
+async function list(req, res) {
+  const { date } = req.query;
+  const data = await service.list(date);
+  console.log(date);
+  console.log(data);
+  res.json({ data: data });
 }
 
 async function create(req, res) {
@@ -40,5 +93,9 @@ async function create(req, res) {
 
 module.exports = {
   list: asyncErrorBoundary(list),
-  create: [hasOnlyValidProperties, asyncErrorBoundary(create)],
+  create: [
+    hasOnlyValidProperties,
+    hasValidTimeAndDate,
+    asyncErrorBoundary(create),
+  ],
 };
