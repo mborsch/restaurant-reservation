a17df8bdc2fb97c9a5dfe358af1f675dccf13350
mborsch <matthewmborschnack@gmail.com>
2022-07-21 18:22:58 -0500

    added status column and fixed issue to add truncate

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,3 +1,4 @@
+/*
 exports.seed = function (knex) {
   return knex("reservations")
     .del()
@@ -55,5 +56,17 @@ exports.seed = function (knex) {
           updated_at: "2020-12-10T08:31:32.326Z",
         },
       ]);
+    });
+};
+
+*/
+
+const reservations = require("./00-reservations.json");
+
+exports.seed = function (knex) {
+  return knex
+    .raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
+    .then(function () {
+      return knex("reservations").insert(reservations);
     });
 };
