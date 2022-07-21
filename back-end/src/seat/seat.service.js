1e1bc65f10997be1f5d6ed5f7e740096c322e8a7
mborsch <matthewmborschnack@gmail.com>
2022-07-21 18:32:33 -0500

    seat backend initial commit

---
+++ back-end/src/seat/seat.service.js
@@ -0,0 +1,47 @@
+const knex = require("../db/connection");
+
+const read = (reservationId) =>
+  knex("reservations")
+    .select("*")
+    .where({ reservation_id: reservationId })
+    .first();
+
+const update = async (tableId, reservationId, updatedTable) => {
+  return knex.transaction(async function (trx) {
+    await trx("tables")
+      .select("*")
+      .where({ table_id: tableId })
+      .update(updatedTable, "*");
+    return await trx("reservations")
+      .select("*")
+      .where({ reservation_id: reservationId })
+      .update({ status: "seated" });
+  });
+};
+
+const finish = async (reservationId) => {
+  return knex.transaction(async function (trx) {
+    await trx("reservations")
+      .where({ reservation_id: reservationId })
+      .update({ status: "finished" });
+    return await trx("tables")
+      .where({ reservation_id: reservationId })
+      .update({ reservation_id: null });
+  });
+};
+
+function list() {
+  return knex("tables").select("*").orderBy("table_name");
+}
+
+function listRes() {
+  return knex("reservations").select("*").orderBy("first_name");
+}
+
+module.exports = {
+  read,
+  update,
+  finish,
+  list,
+  listRes,
+};
