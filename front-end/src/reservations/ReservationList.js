4806e9e1c96e618d920a19597425dc9e793944d9
mborsch <matthewmborschnack@gmail.com>
2022-07-21 18:21:58 -0500

    improved look

--- front-end/src/reservations/ReservationList.js
+++ front-end/src/reservations/ReservationList.js
@@ -3,29 +3,37 @@ import { Link } from "react-router-dom";
 
 const ReservationList = ({ reservations }) => {
   return (
-    <>
-      <div>
-        <ul>
-          {reservations.map((reservation) => (
-            <>
-              <h5>
+    <div className="container col-lg-9">
+      <div className="d-flex flex-column w-500 justify-content-center">
+        {reservations.map((reservation) => (
+          <div className="card card-bg my-2 d-flex align-items-center">
+            <div className="card-header d-flex align-items-center">
+              <h5 className="my-0 mr-2">
                 {reservation.reservation_time}: {reservation.last_name},{" "}
                 {reservation.first_name}
               </h5>
+              <div className="d-flex justify-content-end">
+                <p className="ml-auto my-0">Party of: {reservation.people}</p>
+              </div>
+            </div>
+            <div className="d-inline-block ml-auto justify-content-end mr-3">
               <Link
-                className="btn btn-dark mx-1 mb-2"
+                className="btn btn-dark mr-6 my-2 pl-2 mx-2"
                 to={`/reservations/${reservation.reservation_id}/seat`}
               >
                 <span className="oi oi-people mr-2" />
                 Seat
               </Link>
-              <li>Party of: {reservation.people}</li>
-              <li>Contact: {reservation.mobile_number}</li>
-            </>
-          ))}
-        </ul>
+
+              <button className="btn btn-danger d-lcok d-md-inline mx-2">
+                Cancel
+              </button>
+            </div>
+            {/*  <p>Contact: {reservation.mobile_number}</p> */}
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
