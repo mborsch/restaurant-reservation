d4502bfba257e2ee4d9855d4364e079419a99fa5
mborsch <matthewmborschnack@gmail.com>
2022-07-21 20:58:31 -0500

    fleshed out middleware and creation of create and read

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,20 +1,15 @@
 const service = require("./tables.service");
+const reservationService = require("../reservations/reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const hasProperties = require("../errors/hasProperties");
+const { as } = require("../db/connection");
+const { response } = require("../app");
 
-const VALID_PROPERTIES = ["table_name", "capacity"];
-
-function hasOnlyValidProperties(req, res, next) {
-  const { data = {} } = req.body;
-
-  const invalidFields = Object.keys(data).filter(
-    (field) => !VALID_PROPERTIES.includes(field)
-  );
-
-  if (invalidFields.length) {
+function dataExists(req, res, next) {
+  if (!req.body.data) {
     return next({
       status: 400,
-      message: `Invalid Field(s): ${invalidFields.join(", ")}`,
+      message: `Data missing!`,
     });
   }
   next();
@@ -22,28 +17,67 @@ function hasOnlyValidProperties(req, res, next) {
 
 function tableNameValidator(req, res, next) {
   const { table_name } = req.body.data;
+
+  if (!table_name) {
+    return next({
+      status: 400,
+      message: `missing table_name data.`,
+    });
+  }
   const length = table_name.length;
+  if (!length) {
+    return next({
+      status: 400,
+      message: `table_name can't be left blank.`,
+    });
+  }
 
   if (length >= 2) {
     return next();
   }
   return next({
     status: 400,
-    message: `Invalid table name field. Table name must be at 2 characters or more.`,
+    message: `Invalid table_name field. Table name must be at 2 characters or more.`,
   });
 }
 
 function capacityValidator(req, res, next) {
   const { capacity } = req.body.data;
 
-  if (capacity > 0 && Number.isInteger(capacity)) {
+  if (capacity > 0) {
     return next();
   }
   return next({
     status: 400,
-    message: `Invalid capacity field. Capacity must be greater than 0.`,
+    message: `Invalid capacity field. Capacity (${capacity}) must be greater than 0.`,
   });
 }
+
+function CapacityNaN(req, res, next) {
+  const { capacity } = req.body.data;
+  capacity === Number(capacity)
+    ? next()
+    : next({ status: 400, message: `capacity must be a number.` });
+}
+
+async function tableExists(request, response, next) {
+  const { table_id } = request.params;
+
+  let table = await service.read(table_id);
+
+  const error = { status: 404, message: `Table ${table_id} cannot be found.` };
+
+  if (table) {
+    response.locals.table = table;
+    return next();
+  }
+
+  next(error);
+}
+
+////////////////////////
+//END  MIDDLEWARE
+////////////////////////
 
 async function create(req, res) {
   const data = await service.create(req.body.data);
@@ -55,7 +89,21 @@ async function list(req, res) {
   res.json({ data: data });
 }
 
+async function read(req, res, next) {
+  response.json({
+    data: res.locals.table,
+  });
+}
+
 module.exports = {
   list: asyncErrorBoundary(list),
-  create: [capacityValidator, tableNameValidator, asyncErrorBoundary(create)],
+  tableExists,
+  create: [
+    dataExists,
+    capacityValidator,
+    CapacityNaN,
+    tableNameValidator,
+    asyncErrorBoundary(create),
+  ],
+  read: [asyncErrorBoundary(tableExists), asyncErrorBoundary(read)],
 };
