dd6b9761f38aa7e974a74804d777f533e41fd23b
mborsch <matthewmborschnack@gmail.com>
2022-07-25 17:50:25 -0500

    add update

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -9,8 +9,32 @@ function create(table) {
     .then((createdTable) => createdTable[0]);
 }
 
+async function update(updatedTable, updatedReservation) {
+  const trx = await knex.transaction();
+
+  return trx("tables")
+    .select("*")
+    .where({ table_id: updatedTable.table_id })
+    .update(updatedTable, "*")
+    .then(() => {
+      return trx("reservations")
+        .select("*")
+        .where({ reservation_id: updatedReservation.reservation_id })
+        .update(updatedReservation, "*");
+    })
+    .then(trx.commit)
+    .catch(trx.rollback);
+}
+
 function read(tableId) {
   return knex("tables").select("*").where({ table_id: tableId }).first();
+}
+
+function resRead(reservation_id) {
+  return knex("reservations")
+    .select("*")
+    .where({ reservation_id: reservation_id })
+    .first();
 }
 
 function list() {
@@ -19,6 +43,8 @@ function list() {
 
 module.exports = {
   create,
+  update,
   read,
   list,
+  resRead,
 };
