c49f7fb980c71699c3a3452692050adcfdcb3264
mborsch <matthewmborschnack@gmail.com>
2022-07-25 21:04:00 -0500

    created Search

---
+++ front-end/src/search/Search.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { listReservations } from "../utils/api";
+import ReservationList from "../reservations/ReservationList";
+import ErrorAlert from "../layout/ErrorAlert";
+
+const Search = () => {};
+
+export default Search;
