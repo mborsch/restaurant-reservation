cd9a87cae8c69c99bdedaaff0229c1df3503dcd2
mborsch <matthewmborschnack@gmail.com>
2022-08-05 22:42:27 -0500

    fix api for test

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -10,8 +10,7 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL ||
-  "https://res-reserve-back-end.herokuapp.com";
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
